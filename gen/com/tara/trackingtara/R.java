0f9a0fa4b8982debd1f1c19e39b61b7d0fee41e4
Tara Verity <verityt@accuweather.com>
2013-03-15 14:29:01 -0400

    icons and things

--- gen/com/tara/trackingtara/R.java
+++ gen/com/tara/trackingtara/R.java
@@ -10,22 +10,33 @@ package com.tara.trackingtara;
 public final class R {
     public static final class attr {
     }
+    public static final class bool {
+        /** Enable Activity tracking
+         */
+        public static final int ga_autoActivityTracking=0x7f050000;
+        public static final int ga_debug=0x7f050002;
+        /** Enable automatic exception tracking
+         */
+        public static final int ga_reportUncaughtExceptions=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f040001;
+        public static final int ga_trackingId=0x7f040000;
+        public static final int hello_world=0x7f040002;
+        public static final int menu_settings=0x7f040003;
+        public static final int tracking_tara=0x7f040004;
     }
     public static final class style {
         /** 
@@ -49,10 +60,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
